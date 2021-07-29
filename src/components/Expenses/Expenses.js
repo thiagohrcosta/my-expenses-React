ee5bc7699d6a3261307938c56051589d858e0e00
Thiago Costa <thiagohrcosta86@gmail.com>
2021-07-29 16:04:04 -0300

    Project Completed

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
 
@@ -24,6 +25,7 @@ const Expenses = (props) =>{
         selected={filteredYear} 
         onChangeFilter={filterChangeHanlder} 
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />              
       </Card>
     </div>
